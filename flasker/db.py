62fbdd15e66ef48a83eac67ad06ba87aea699a57
Bendik Knapstad <knaben@OBOS.NO>
2019-10-03 23:04:14 +0200

    added auth.py

--- flasker/db.py
+++ flasker/db.py
@@ -5,7 +5,7 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 def get_db():
-    if "db" not i g:
+    if "db" not in g:
         g.db = sqlite3.connect(
             current_app.config["DATABASE"],
             detect_types = sqlite3.PARSE_DECLTYPES
@@ -24,3 +24,15 @@ def init_db():
 
     with current_app.open_resource("schema.sql") as f:
         db.executescript(f.read().decode("utf8"))
+
+@click.command("init-db")
+@with_appcontext
+def init_db_comand():
+    "clear the existing data and create tables"
+    init_db()
+    click.echo("Database initialized")
+    
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_comand)
