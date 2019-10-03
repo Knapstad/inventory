664c7850cb2818be31cc497e86c5e8f16a11fd89
Bendik Knapstad <knaben@OBOS.NO>
2019-10-03 22:29:41 +0200

    add __init__.py example app

---
+++ flasker/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import flask
+
+def create_app(test_config=None):
+    #this wil create and test the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = "dev",
+        DATABASE = os.path.join(app.instance_path, "flasker.sqlite")
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # Make shure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # a simple page that says Hello
+    @app.route("/hello")
+    def hello():
+        return "Hello"
+
+    return app
